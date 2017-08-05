9550287b6987bad891eb523792686420a07db1ae
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2017-08-05 10:23:10 +0200

    Fixed dump variable, minor fixes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Introduction 
 This is a simple collection of vsts build/release tasks that contains various utilities. 
+
+A better readme is contained in the [src readme](src/README.md)
     
--- src/tasks/dumpenv/dump.ps1
+++ src/tasks/dumpenv/dump.ps1
@@ -10,14 +10,16 @@ try {
    $out = ""
    Foreach ($v in $var) {$out = $out + "`t{0,-28} = {1,-28}`n" -f $v.Name, $v.Value}
 
-   $fileName = [System.IO.Path]::GetTempFileName();
-   Write-Output "dump variables on $fileName"
-   Set-Content $fileName $out
+   #$fileName = [System.IO.Path]::GetTempFileName() + ".md";
+   $fileName = "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\environment_variables.md"
+   Write-Output "--------------------- VARIABLE NAMES ----------------------------"
+   Write-Output $out
 
+   Write-Output "Dumping variables on $fileName"
+   $out | Out-File -FilePath $fileName -Force
+
+   Write-Output "Attaching report to build with command ##vso[task.addattachment type=Distributedtask.Core.Summary;name=Environment Variables;]$fileName"
    Write-Output "##vso[task.addattachment type=Distributedtask.Core.Summary;name=Environment Variables;]$fileName"
-
-   # Output the message to the log.
-   Write-Output (Get-VstsInput -Name msg)
 } finally {
     Trace-VstsLeavingInvocation $MyInvocation
 }
